b7bffd63bf2326412c0b4c4dc4e8fdbe0d23d69d
cmendoza <carlos.mendozacarrero@gmail.com>
2021-02-18 22:54:55 -0300

    Mi primera aplicacion con Django, primer commit

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Post
+#Para hacer nuestro modelo visible en la página del administrador, tenemos que registrar el modelo con 
+#admin.site.register(Post).
+admin.site.register(Post)
+# Register your models here.
